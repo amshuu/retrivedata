2f3fb3927ca266c0e188348f89135e881bb44328
amshuu <107336457+amshuu@users.noreply.github.com>
2023-09-30 14:38:55 +0530

    Add files via upload

---
+++ code.js
@@ -0,0 +1,46 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const dataForm = document.getElementById("data-form");
+    const storeButton = document.getElementById("store-button");
+    const retrieveForm = document.getElementById("retrieve-form");
+    const retrieveButton = document.getElementById("retrieve-button");
+    const retrieveResult = document.getElementById("retrieve-result");
+
+    // Data storage object
+    const dataStorage = {};
+
+    // Event listener for storing data
+    storeButton.addEventListener("click", function (e) {
+        e.preventDefault();
+        const dataInput = document.getElementById("data");
+        const codeInput = document.getElementById("code");
+        const data = dataInput.value.trim();
+        const code = codeInput.value.trim();
+
+        if (data !== "" && code !== "" && !isNaN(code) && code.length === 5) {
+            dataStorage[code] = data;
+            dataInput.value = "";
+            codeInput.value = "";
+            alert("Data stored successfully!");
+        } else {
+            alert("Invalid data or code. Please check and try again.");
+        }
+    });
+
+    // Event listener for retrieving data
+    retrieveButton.addEventListener("click", function (e) {
+        e.preventDefault();
+        const retrieveCodeInput = document.getElementById("retrieve-code");
+        const retrieveCode = retrieveCodeInput.value.trim();
+
+        if (retrieveCode !== "" && !isNaN(retrieveCode) && retrieveCode.length === 5) {
+            if (dataStorage.hasOwnProperty(retrieveCode)) {
+                const retrievedData = dataStorage[retrieveCode];
+                retrieveResult.textContent = `Retrieved data: ${retrievedData}`;
+            } else {
+                retrieveResult.textContent = "Code not found. Please enter a valid code.";
+            }
+        } else {
+            retrieveResult.textContent = "Invalid code. Please enter a 5-digit numeric code.";
+        }
+    });
+});
